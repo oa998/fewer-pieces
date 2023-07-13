cfab85ed9546f051cda935752327d0a06a23b70a
zzz sss <onlineaccounts998@gmail.com>
2023-07-13 18:26:55 -0400

    auto commit

--- src/components/modal.svelte
+++ src/components/modal.svelte
@@ -2,14 +2,13 @@
 	let modal: HTMLDialogElement;
 
 	export let onClose: () => void;
-	export let isOpen: string | boolean;
+	export let isOpen: boolean;
 
 	$: if (modal) {
 		document.addEventListener('keydown', (ev) => {
-			if (ev.key === 'Escape' && modal.open) {
+			if (ev.key === 'Escape') {
 				console.log('clicked outside');
 				modal.close();
-				onClose();
 			}
 		});
 		modal.addEventListener('click', (e) => {
@@ -19,39 +18,58 @@
 				e.clientX > dims.right ||
 				e.clientY < dims.top ||
 				e.clientY > dims.bottom;
-			if (clickedOutside && modal.open) {
-				console.log('clicked outside');
+			if (clickedOutside) {
 				modal.close();
-				onClose();
 			}
 		});
-		if (isOpen && !modal.open) {
+		if (isOpen) {
 			modal.showModal();
-		} else if (!isOpen && modal.open) {
+		} else if (!isOpen) {
 			modal.close();
-			onClose();
 		}
 	}
 </script>
 
-<dialog data-modal bind:this={modal}>
+<dialog open={!!isOpen} on:close={onClose} data-modal bind:this={modal}>
 	<div>
-		<button class="close-card" on:click={onClose}>✕</button>
+		<form method="dialog">
+			<button class="close-card">x</button>
+		</form>
 		<slot />
 	</div>
 </dialog>
 
 <style>
-	dialog {
-		border-radius: 10px;
-		@apply bg-gray-900;
-	}
-	dialog::backdrop {
-		background-color: hsla(0, 0%, 0%, 0.7);
-	}
 	.close-card {
 		border-radius: 3px;
 		line-height: 10px;
 		@apply absolute top-2 right-2 text-yellow-300 border border-yellow-700 p-1 outline-none;
+	}
+
+	dialog {
+		/* display: block;
+		pointer-events: none;
+		opacity: 0;
+		transition: all 3.5s;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center; */
+		border-radius: 10px;
+		@apply bg-gray-900;
+	}
+	dialog[open] {
+		animation: myFadeIn 5s ease normal;
+	}
+	@keyframes myFadeIn {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+	dialog::backdrop {
+		background-color: hsla(0, 0%, 0%, 0.7);
 	}
 </style>
